afec3f1d1d72ad40982e7fc43fd39fb83a7f97b1
Solomon White <rubysolo@gmail.com>
2011-05-22 12:56:12 -0600

    filehandle doesn't need to accessible from outside

--- lib/cutie/cutie.rb
+++ lib/cutie/cutie.rb
@@ -1,6 +1,6 @@
 class Cutie
   DEBUG = $stdout
-  attr_accessor :filehandle, :root
+  attr_accessor :root
 
   def initialize(fh, debug=false)
     @debug      = debug
@@ -34,7 +34,7 @@ class Cutie
       if atom.container?
         debug "pushing container atom onto the stack"
         @stack << atom
-      elsif filehandle.pos == @stack.last.next_position
+      elsif @filehandle.pos == @stack.last.next_position
         debug "popping last container from the stack"
         @stack.pop.close
       end
@@ -45,7 +45,7 @@ class Cutie
 
   def dump_atoms
     indent = 0
-    filehandle.rewind
+    @filehandle.rewind
 
     while atom = next_atom
       print "  " * indent rescue nil
@@ -53,7 +53,7 @@ class Cutie
 
       if atom.container?
         indent += 1
-      elsif filehandle.pos == atom.next_position
+      elsif @filehandle.pos == atom.next_position
         indent -= 1
       end
     end
@@ -61,17 +61,17 @@ class Cutie
 
 
   def next_atom
-    start_position = filehandle.pos
+    start_position = @filehandle.pos
 
-    if bytes = filehandle.read(8)
+    if bytes = @filehandle.read(8)
       raise "Expected 8 bytes, got #{ bytes.length }" unless bytes.length == 8
 
       size, format = *bytes.unpack('NA4')
-      atom = Atom.init(filehandle, start_position, size, format)
+      atom = Atom.init(@filehandle, start_position, size, format)
 
       # check for extended or invalid atom size
       if atom.size == 1
-        atom.size = filehandle.read(8).unpack('Q').first # TODO : force big-endian?
+        atom.size = @filehandle.read(8).unpack('Q').first # TODO : force big-endian?
       end
 
       raise "invalid atom size #{ atom.size }" if atom.size < 0
